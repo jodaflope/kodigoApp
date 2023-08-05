8ee4d1cf33cabbe7c07132639099db46e671f8a9
Jonatan Flores <jonatandavidfloresperez@gmail.com>
2023-08-05 14:58:31 -0600

    :sparkles: feat (Templates): add Templates files

--- Views/bootcampView.php
+++ Views/bootcampView.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kodigoApp</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
+<?php require ('templates/Header.php');?>
     <h1 class="text-center">Welcome to kodigoApp</h1>
     <h2 class="text-center">All Bootcamps</h2>
     <div class="container">
@@ -27,10 +19,4 @@
 
         </div>
     </div>
-
-    
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require ('templates/Footer.php');?>
